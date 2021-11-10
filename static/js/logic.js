2d454bda6267260217c9c33a99b8037fbdc1a93c
shantaljambotkar <88519257+shantaljambotkar@users.noreply.github.com>
2021-11-10 13:59:54 -0500

    Update to branch

--- static/js/logic.js
+++ static/js/logic.js
@@ -13,3 +13,5 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+console.log("working check point 2");
